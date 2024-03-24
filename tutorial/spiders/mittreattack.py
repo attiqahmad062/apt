0451ebd82f4c68f4900c949cb90184465734001a
attiqahmad062 <102343526+attiqahmad062@users.noreply.github.com>
2024-03-25 00:14:55 +0500

    refferences crawling done

--- tutorial/spiders/mittreattack.py
+++ tutorial/spiders/mittreattack.py
@@ -38,32 +38,47 @@ class MITREAttackSpider(scrapy.Spider):
             technique_url = row.css('td:nth-child(2) a::attr(href)').get()
             references_tag=''
             # Checking the length of the row to determine the correct column for 'sub_id_data'
-            if len(row.css('td')) < 5:
+            references=[]
+            if len(row.css('td')) >= 5:
                 sub_id_data = row.css('td:nth-child(3) a::text').get()
                 name_data = ' '.join(row.css('td:nth-child(4) *::text').getall()).strip()
                 use_data = ' '.join(row.css('td:nth-child(5) *::text').getall()).strip()
-                references_tag='td:nth-child(5) a'
+                references_tag='td:nth-child(5) a' 
+                for link in response.css(references_tag):
+                    # Extract the href attribute
+                        href = link.css('::attr(href)').get()
+                        # Extract the text content of the <a> tag
+                        text = link.css('::text').get()
+                
+                    # Check if the text matches the pattern of numbers in square brackets
+                        if text and text.strip().startswith('[') and text.strip().endswith(']'):
+                        # This is a reference link, extract it
+                          if href not in references:
+                            references.append(href)
             else:
                 sub_id_data = None
                 name_data = ' '.join(row.css('td:nth-child(3) *::text').getall()).strip()
                 use_data = ' '.join(row.css('td:nth-child(4) *::text').getall()).strip()
                 references_tag='td:nth-child(4) a'
+                for link in response.css(references_tag):
+                # Extract the href attribute
+                    href = link.css('::attr(href)').get()
+                    # Extract the text content of the <a> tag
+                    text = link.css('::text').get()
+            
+                # Check if the text matches the pattern of numbers in square brackets
+                    if text and text.strip().startswith('[') and text.strip().endswith(']'):
+                    # This is a reference link, extract it
+                      if href not in references: 
+                        references.append(href)
             technique_url = response.urljoin(technique_url.strip()) if technique_url else None
-            for link in response.css(references_tag):
-            # Extract the href attribute
-                href = link.css('::attr(href)').get()
-                # Extract the text content of the <a> tag
-                text = link.css('::text').get()
-            references=[]
-            # Check if the text matches the pattern of numbers in square brackets
-            if text and text.strip().startswith('[') and text.strip().endswith(']'):
-                # This is a reference link, extract it
-                references.append(href)
+            
+
            
             yield ( {
                 'Domain': domain_data.strip() if domain_data else None,
                 'ID': id_data.strip() if id_data else None,
-                # 'SubID': sub_id_data.strip() if sub_id_data else None,
+                'SubID': sub_id_data.strip() if sub_id_data else None,
                 # 'Name': name_data if name_data else None,
                 'Use': use_data if use_data else None,
                 "references":references
--- tutorial/testdb.py
+++ tutorial/testdb.py
@@ -15,10 +15,9 @@ def main():
         conn = mysql.connector.connect(**MYSQL_SETTINGS)
         if conn.is_connected():
             print("Connected to MySQL database")
-        cursor = conn.cursor()   
+        cursor = conn.cursor() 
         cursor.execute("select * from etiapt.sub_id")
         rows=cursor.fetchall()
-        
         for row in rows:
             print(row)
         # Perform database operations here...
