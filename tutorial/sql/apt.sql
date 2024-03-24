439181a86bbc354585cb78875c77556515c70991
attiqahmad062 <102343526+attiqahmad062@users.noreply.github.com>
2024-03-24 16:01:26 +0500

    sql push

--- tutorial/sql/apt.sql
+++ tutorial/sql/apt.sql
@@ -1,20 +1,15 @@
-use etiapt;
-CREATE TABLE software_used (
-    id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
-    reference VARCHAR(255),
-    techniques VARCHAR(255)
-);
-CREATE TABLE sub_id (
-    id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255)
-);
+use  etiapt;
+select * from apt_group;
+select * from apt_group_techniques;
+select * from sub_id;
+select * from software_used;
 
--- Procedure Examples 
-CREATE TABLE procedure_example (
-    id VARCHAR(255)  PRIMARY KEY,
-    name VARCHAR(255),
-    description Varchar(255),
-    reference varchar(255)
-); 
-	
+select * from etiapt
+SET SQL_SAFE_UPDATES = 0;
+SET FOREIGN_KEY_CHECKS = 0;
+-- -- Delete the table if it exists
+DELETE FROM apt_group;
+DELETE FROM apt_group_techniques;
+SET FOREIGN_KEY_CHECKS = 1;
+-- Re-enable safe mode
+SET SQL_SAFE_UPDATES = 1;
