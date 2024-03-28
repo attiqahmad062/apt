45d3a0239abb81043c8b37280b1789e78a7b904f
Husnain <husnainahmed971@gmail.com>
2024-03-28 23:36:41 +0500

    it is pointing the correct location now on the page, but crawling nothing.

--- tutorial/spiders/mittreattack.py
+++ tutorial/spiders/mittreattack.py
@@ -29,21 +29,21 @@ class MITREAttackSpider(scrapy.Spider):
             if column1_url_absolute:
               yield response.follow(column1_url_absolute, self.parse_group_page)
     def parse_group_page(self, response):
-                # Extract the ID
-        id_info = response.xpath('//*[@id="v-attckmatrix"]/div[2]/div/div/div/div[1]/div[2]/div[1]/div')
-        
+       # Extract the ID
+        id_info = ''.join(response.css('#card-id .h5.card-title + div *::text').getall()).strip()
+
         # Extract the Contributors
-        contributors = response.css('div.row.card-data:nth-child(2) span.h5.card-title::text').getnext().strip()
-        
+        contributors = ''.join(response.css('.row.card-data:nth-child(2) .h5.card-title + div *::text').getall()).strip()
+
         # Extract the Version
-        version = response.css('div.row.card-data:nth-child(3) span.h5.card-title::text').getnext().strip()
-        
+        version = ''.join(response.css('.row.card-data:nth-child(3) .h5.card-title + div *::text').getall()).strip()
+
         # Extract the Created date
-        created = response.css('div.row.card-data:nth-child(4) span.h5.card-title::text').getnext().strip()
-        
+        created = ''.join(response.css('.row.card-data:nth-child(4) .h5.card-title + div *::text').getall()).strip()
+
         # Extract the Last Modified date
-        last_modified = response.css('div.row.card-data:nth-child(5) span.h5.card-title::text').getnext().strip()
-        
+        last_modified = ''.join(response.css('.row.card-data:nth-child(5) .h5.card-title + div *::text').getall()).strip()
+
         yield {
             'ID': id_info,
             'Contributors': contributors,
@@ -51,6 +51,8 @@ class MITREAttackSpider(scrapy.Spider):
             'Created': created,
             'Last Modified': last_modified
         }
+
+
 
 
 
@@ -94,103 +96,103 @@ class MITREAttackSpider(scrapy.Spider):
             # # if technique_url:
             #     yield response.follow(technique_url, self.parse_techniques)
             
-        # Software Table:
-        softwareTable = response.css('table.table-alternate tr')
-        for index, row in enumerate(softwareTable, start=1):
-            # Extracting data from each column in the row
-            id_data = ' '.join(row.css('td:nth-child(1) *::text').getall()).strip()
-            name_data = ' '.join(row.css('td:nth-child(2) *::text').getall()).strip()
-            # references_data = ' '.join(row.css('td:nth-child(3) *::text').getall()).strip()
-            references_data = row.css('td:nth-child(3) span sup a::attr(href)').get()
-            # Extracting techniques
-            techniques_data = []
-            techniques_nodes = row.css('td:nth-child(4) *::text').getall()
-            for node in techniques_nodes:
-                techniques_data.append(node.strip())
-            # Check if ID starts with 'S'
-            if id_data and id_data.startswith('S') and id_data[1:].isdigit():
-                yield SoftwareTable( {
-                    'ID': id_data if id_data else None,
-                    'Name': name_data if name_data else None,
-                    'References': references_data if references_data else None,
-                    'Techniques': ' '.join(techniques_data) if techniques_data else None,
-                } )
-        # campaigns 
-        if response.css('h2#campaigns'):
-            for row in response.xpath('//*[@id="v-attckmatrix"]/div[2]/div/div/div/div[3]'):
-                yield  CompainsTable({
-                    'ID': row.css('td:nth-child(1) a::text').get(),
-                    'Name': row.css('td:nth-child(2) a::text').get(),
-                    'FirstSeen': row.css('td:nth-child(3) *::text').get(),
-                    'LastSeen': row.css('td:nth-child(4) *::text').get(),
-                    'References': row.css('td:nth-child(5)  p sup a::attr(href)').get(),
-                     'Techniques': row.css('td:nth-child(6) a::attr(href)').getall(),
-                })
-        #associated groups (aliasDescription)
-        if response.css('h2#aliasDescription'):
-            for row in response.xpath('//*[@id="v-attckmatrix"]/div[2]/div/div/div/div[2]/table/tbody/tr'):
-                name = row.xpath('./td[1]/text()').get()
-                cleaned_name = re.sub(r'\W+', '', name) if name else name
-                description = row.xpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div/div/div/div[2]/table/tbody/tr[1]/td[2]/p/span/sup/a/@href').get()
-                yield {
-                    'Name': cleaned_name,
-                    'Description': description
-                }
-#     def parse_techniques(self, response):
-#         #  subtechniques
-#         for row in response.xpath('//div[@id="subtechniques-card-body"]//table//tbody/tr'):
-#             # yield SubTechniques( {
-#             #     'ID': row.xpath('td[1]/a/text()').get(),
-#             #     'Name': row.xpath('td[2]/a/text()').get(),
-#             # })
+#         # Software Table:
+#         softwareTable = response.css('table.table-alternate tr')
+#         for index, row in enumerate(softwareTable, start=1):
+#             # Extracting data from each column in the row
+#             id_data = ' '.join(row.css('td:nth-child(1) *::text').getall()).strip()
+#             name_data = ' '.join(row.css('td:nth-child(2) *::text').getall()).strip()
+#             # references_data = ' '.join(row.css('td:nth-child(3) *::text').getall()).strip()
+#             references_data = row.css('td:nth-child(3) span sup a::attr(href)').get()
+#             # Extracting techniques
+#             techniques_data = []
+#             techniques_nodes = row.css('td:nth-child(4) *::text').getall()
+#             for node in techniques_nodes:
+#                 techniques_data.append(node.strip())
+#             # Check if ID starts with 'S'
+#             if id_data and id_data.startswith('S') and id_data[1:].isdigit():
+#                 yield SoftwareTable( {
+#                     'ID': id_data if id_data else None,
+#                     'Name': name_data if name_data else None,
+#                     'References': references_data if references_data else None,
+#                     'Techniques': ' '.join(techniques_data) if techniques_data else None,
+#                 } )
+#         # campaigns 
+#         if response.css('h2#campaigns'):
+#             for row in response.xpath('//*[@id="v-attckmatrix"]/div[2]/div/div/div/div[3]'):
+#                 yield  CompainsTable({
+#                     'ID': row.css('td:nth-child(1) a::text').get(),
+#                     'Name': row.css('td:nth-child(2) a::text').get(),
+#                     'FirstSeen': row.css('td:nth-child(3) *::text').get(),
+#                     'LastSeen': row.css('td:nth-child(4) *::text').get(),
+#                     'References': row.css('td:nth-child(5)  p sup a::attr(href)').get(),
+#                      'Techniques': row.css('td:nth-child(6) a::attr(href)').getall(),
+#                 })
+#         #associated groups (aliasDescription)
+#         if response.css('h2#aliasDescription'):
+#             for row in response.xpath('//*[@id="v-attckmatrix"]/div[2]/div/div/div/div[2]/table/tbody/tr'):
+#                 name = row.xpath('./td[1]/text()').get()
+#                 cleaned_name = re.sub(r'\W+', '', name) if name else name
+#                 description = row.xpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div/div/div/div[2]/table/tbody/tr[1]/td[2]/p/span/sup/a/@href').get()
+#                 yield {
+#                     'Name': cleaned_name,
+#                     'Description': description
+#                 }
+# #     def parse_techniques(self, response):
+# #         #  subtechniques
+# #         for row in response.xpath('//div[@id="subtechniques-card-body"]//table//tbody/tr'):
+# #             # yield SubTechniques( {
+# #             #     'ID': row.xpath('td[1]/a/text()').get(),
+# #             #     'Name': row.xpath('td[2]/a/text()').get(),
+# #             # })
             
-#         # procedure examples
-#         if response.css('h2#examples'):
-#                 rows = response.xpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div/div/div/div[2]/table')
-#                 for row in rows:
-#                         # Extract the data from each cell in the row
-#                         id = row.css('td:nth-child(1) a::text').get()
-#                         name = row.css('td:nth-child(2) a::text').get()
-#                         description = row.css('td:nth-child(3) p::text').get()
+# #         # procedure examples
+# #         if response.css('h2#examples'):
+# #                 rows = response.xpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div/div/div/div[2]/table')
+# #                 for row in rows:
+# #                         # Extract the data from each cell in the row
+# #                         id = row.css('td:nth-child(1) a::text').get()
+# #                         name = row.css('td:nth-child(2) a::text').get()
+# #                         description = row.css('td:nth-child(3) p::text').get()
                         
                     
-#                         yield ProcedureExamples( {
-#                             'ID': id,
-#                             'Name': name,
-#                             'Description': description
-#                     })
-#         #mitigations
-#         # if response.css('h2#mitigations'):
-#         #     rows = response.xpath('//*[@id="v-attckmatrix"]/div[2]/div/div/div/div[3]/table')
-#         #     for row in rows: 
-#         #         id = row.css('td:nth-child(1) a::text').get()
-#         #         mitigation = row.css('td:nth-child(2) a::text').get()
-#         #         description = row.css('td:nth-child(3) p::text').get()
-#         #         mitigation_url = row.css('td:nth-child(2) a::attr(href)').get()
-#         #         technique_url=response.urljoin(technique_url.strip()) if technique_url else None
-#         #         if id.__contains__('M'):
+# #                         yield ProcedureExamples( {
+# #                             'ID': id,
+# #                             'Name': name,
+# #                             'Description': description
+# #                     })
+# #         #mitigations
+# #         # if response.css('h2#mitigations'):
+# #         #     rows = response.xpath('//*[@id="v-attckmatrix"]/div[2]/div/div/div/div[3]/table')
+# #         #     for row in rows: 
+# #         #         id = row.css('td:nth-child(1) a::text').get()
+# #         #         mitigation = row.css('td:nth-child(2) a::text').get()
+# #         #         description = row.css('td:nth-child(3) p::text').get()
+# #         #         mitigation_url = row.css('td:nth-child(2) a::attr(href)').get()
+# #         #         technique_url=response.urljoin(technique_url.strip()) if technique_url else None
+# #         #         if id.__contains__('M'):
                
-#         #            yield Mitigations({
-#         #             'ID': id,
-#         #             'Mitigation': mitigation,
-#         #             'Description': description
-#         #         })
-#         #detections
-#         # if response.css('h2#detection'):
-#         #      rows = response.css('table.table.datasources-table.table-bordered tbody tr')
-#         #      for row in rows:
-#         #         # Extract the data from each cell in the row
-#         #         id = row.css('td:nth-child(1) a::text').get()
-#         #         data_source = row.css('td:nth-child(2) a::text').get()
-#         #         data_component = row.css('td:nth-child(3) a::text').get()
-#         #         detects = row.css('td:nth-child(4) p::text').get()
-#         #         # Yield the extracted   data
-#         #         yield   {
-#         #             'ID': id,
-#         #             'DataSource': data_source,
-#         #             'DataComponent': data_component,
-#         #             'Detects': detects
-#         #         }
-# #
-# # 
+# #         #            yield Mitigations({
+# #         #             'ID': id,
+# #         #             'Mitigation': mitigation,
+# #         #             'Description': description
+# #         #         })
+# #         #detections
+# #         # if response.css('h2#detection'):
+# #         #      rows = response.css('table.table.datasources-table.table-bordered tbody tr')
+# #         #      for row in rows:
+# #         #         # Extract the data from each cell in the row
+# #         #         id = row.css('td:nth-child(1) a::text').get()
+# #         #         data_source = row.css('td:nth-child(2) a::text').get()
+# #         #         data_component = row.css('td:nth-child(3) a::text').get()
+# #         #         detects = row.css('td:nth-child(4) p::text').get()
+# #         #         # Yield the extracted   data
+# #         #         yield   {
+# #         #             'ID': id,
+# #         #             'DataSource': data_source,
+# #         #             'DataComponent': data_component,
+# #         #             'Detects': detects
+# #         #         }
+# # #
+# # # 
         
