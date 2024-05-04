4a512461ac3eaca31da157cbe6c477bef038b763
attiqahmad062 <102343526+attiqahmad062@users.noreply.github.com>
2024-05-04 14:07:43 +0500

    sql changed

--- tutorial/sql/apt.sql
+++ tutorial/sql/apt.sql
@@ -1,15 +1,5 @@
-use  etiapt;
+use etiapt;
 select * from apt_group;
-select * from apt_group_techniques;
-select * from sub_id;
-select * from software_used;
-
-select * from etiapt
-SET SQL_SAFE_UPDATES = 0;
-SET FOREIGN_KEY_CHECKS = 0;
--- -- Delete the table if it exists
-DELETE FROM apt_group;
-DELETE FROM apt_group_techniques;
-SET FOREIGN_KEY_CHECKS = 1;
--- Re-enable safe mode
-SET SQL_SAFE_UPDATES = 1;
+-- set sql_safe_updates=0;
+-- delete from apt_group;
+-- set sql_safe_updates=0;
